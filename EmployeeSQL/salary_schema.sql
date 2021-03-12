a85572d24d227e9070853246c39e5d1456dbc19e
JA Gorman <jagorman42@gmail.com>
2021-03-12 17:01:25 -0600

    added all tables and csv files

---
+++ EmployeeSQL/salary_schema.sql
@@ -0,0 +1,10 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/cFM1gf
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "salaries" (
+    "emp_no" varchar(10)   NOT NULL,
+    "salary" varchar(10)   NOT NULL
+);
+
