bba7be41ee9fa475a55b75feb8bc00f369097c89
JA Gorman <jagorman42@gmail.com>
2021-03-16 22:41:41 -0500

    edited files, started bonus

---
+++ EmployeeSQL_Schema/employees_schema.sql
@@ -0,0 +1,15 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/cFM1gf
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "employees" (
+    "emp" varchar(10)   NOT NULL,
+    "emp_title" varchar(10)   NOT NULL,
+    "birth_date" varchar(10)   NOT NULL,
+    "first_name" varchar(20)   NOT NULL,
+    "last_name" varchar(20)   NOT NULL,
+    "sex" varchar(1)   NOT NULL,
+    "hire_date" date   NOT NULL
+);
+
---
+++ EmployeeSQL_Schema/title_id_schema.sql
@@ -0,0 +1,10 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/cFM1gf
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "title_id" (
+    "title_id" varchar(10)   NOT NULL,
+    "title" varchar(20)   NOT NULL
+);
+
